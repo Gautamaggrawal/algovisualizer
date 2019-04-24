067464dc19736b5bd8f951d190d5d05da507606b
Gautam <gautamaggrawalsd@yahoo.in>
2019-04-25 01:16:44 +0530

    updates

--- search/views.py
+++ search/views.py
@@ -10,7 +10,6 @@ def SortingView(request):
     arr=(request.POST.getlist('d[]'))
     n = len(arr) 
     quickSort(arr,0,n-1)
-    print(arr)
     return JsonResponse({'sorteddata':arr})
 
   return render(request,'search.html')
