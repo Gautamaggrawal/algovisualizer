94c320234dc0ff1aa8a3e74f7b2184d2dc19334d
gautamaggrawal <Gautamaggrawalsd@yahoo.in>
2019-04-24 12:47:46 +0530

    Update views.py

--- search/views.py
+++ search/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .qsort import *
+
 
 # Create your views here.
+@csrf_exempt
 def searchhome(request):
-	return render(request,'search.html')
+  if request.method == "POST":
+    arr=(request.POST.getlist('d[]'))
+    n = len(arr) 
+    quickSort(arr,0,n-1)
+    print(arr)
+    return JsonResponse({'sorteddata':arr})
+
+  return render(request,'search.html')
 
